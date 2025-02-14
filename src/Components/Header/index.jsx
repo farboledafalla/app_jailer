ac4c0885e84772d714170125c76158da4ae6f566
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2025-02-13 20:49:39 -0500

    Se ajusta el Sidebar con NavLink para poner estilos a la ruta activa

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
    return (
-      <header className='bg-white shadow-md p-4 flex justify-between items-center'>
+      <header className='bg-white shadow-md p-4 flex justify-between items-center rounded-lg'>
          <h1 className='text-xl font-semibold'>Panel de Administración</h1>
          <button className='bg-red-500 text-white px-4 py-2 rounded'>
             Cerrar Sesión
--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,43 +1,56 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Home, BookOpen, Users, Settings } from 'lucide-react';
 
 export const Sidebar = () => {
+   //Variables con estilos para rutas activa/inactiva
+   const activeStyle = 'bg-gray-700';
+   const normalStyle =
+      'flex items-center gap-3 p-2 rounded-lg hover:bg-gray-500';
+
    return (
       <aside className='w-64 bg-gray-800 text-white h-screen p-5'>
          <h2 className='text-xl font-bold mb-6'>Admin Dashboard</h2>
          <nav>
             <ul>
                <li className='mb-4'>
-                  <Link
+                  <NavLink
                      to='/'
-                     className='flex items-center gap-3 p-2 rounded hover:bg-gray-700'
+                     className={({ isActive }) =>
+                        `${normalStyle} ${isActive ? activeStyle : undefined}`
+                     }
                   >
                      <Home size={20} /> Inicio
-                  </Link>
+                  </NavLink>
                </li>
                <li className='mb-4'>
-                  <Link
+                  <NavLink
                      to='/courses'
-                     className='flex items-center gap-3 p-2 rounded hover:bg-gray-700'
+                     className={({ isActive }) =>
+                        `${normalStyle} ${isActive ? activeStyle : undefined}`
+                     }
                   >
                      <BookOpen size={20} /> Cursos
-                  </Link>
+                  </NavLink>
                </li>
                <li className='mb-4'>
-                  <Link
+                  <NavLink
                      to='/users'
-                     className='flex items-center gap-3 p-2 rounded hover:bg-gray-700'
+                     className={({ isActive }) =>
+                        `${normalStyle} ${isActive ? activeStyle : undefined}`
+                     }
                   >
                      <Users size={20} /> Usuarios
-                  </Link>
+                  </NavLink>
                </li>
                <li>
-                  <Link
+                  <NavLink
                      to='/settings'
-                     className='flex items-center gap-3 p-2 rounded hover:bg-gray-700'
+                     className={({ isActive }) =>
+                        `${normalStyle} ${isActive ? activeStyle : undefined}`
+                     }
                   >
                      <Settings size={20} /> Configuración
-                  </Link>
+                  </NavLink>
                </li>
             </ul>
          </nav>
