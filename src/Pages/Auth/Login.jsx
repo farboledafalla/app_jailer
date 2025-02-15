a44aa17b18f64987c5953a39d94d1c6cdc9456c2
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2025-02-14 20:00:09 -0500

    Se ajusta componente Register y funciona bien

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,23 +1,60 @@
+import { useState, useRef } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import { LayoutAuth } from '../../Components/Layout/LayoutAuth';
+import { useFocusInput } from '../../utils/focusInput';
+
+import axios from 'axios';
 
 export const Login_ = () => {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const navigate = useNavigate();
+   const location = useLocation();
+
+   // Obtiene la ruta desde donde viene, si no existe deja la raiz
+   const from = location.state?.from?.pathname || '/';
+
+   //Referencia al input que tendrá el foco
+   const usernameRef = useRef(null);
+   useFocusInput(usernameRef);
+
+   const handleLogin = async (e) => {
+      e.preventDefault();
+      try {
+         const response = await axios.post('http://localhost:3001/api/login', {
+            username,
+            password,
+         });
+         localStorage.setItem('token', response.data.token);
+         console.log(response.data);
+         // context.setLoggedUser(UserInfo());
+
+         //Navega a la ruta desde doncde intentó ingresar
+         navigate(from, { replace: true });
+      } catch (error) {
+         console.error('Error en el login: ', error);
+         alert(error.response.data.error);
+      }
+   };
+
    return (
       <LayoutAuth>
          <div className='bg-white my-8 p-4 rounded-lg shadow-md border border-gray-200 hover:shadow-lg transition w-96'>
             <div className='flex flex-col justify-center items-center'>
                <h1 className='text-3xl font-extrabold mb-5'>Login</h1>
-               <form onSubmit=''>
+               <form onSubmit={handleLogin}>
                   <div>
                      <label className='flex flex-col'>
                         Username
                         <input
                            type='text'
                            id='username'
-                           // value={username}
-                           // onChange={(e) => setUsername(e.target.value)}
+                           value={username}
+                           onChange={(e) => setUsername(e.target.value)}
                            required
                            autoComplete='username'
-                           // ref={usernameRef}
+                           ref={usernameRef}
                            className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
                         />
                      </label>
@@ -28,8 +65,8 @@ export const Login_ = () => {
                         <input
                            type='password'
                            id='password'
-                           // value={password}
-                           // onChange={(e) => setPassword(e.target.value)}
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}
                            required
                            autoComplete='current-password'
                            className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
